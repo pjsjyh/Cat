c872892bcd8ed78b0af866ac8960bf6056fbe24b
pjsjyh <pjsjyh@naver.com>
2025-07-22 20:49:38 +0900

    캐릭터 데이터 연결 및 기초 데이터 작업

--- Cat/Assets/Scripts/FurnitureScript/FurnitureController.cs
+++ Cat/Assets/Scripts/FurnitureScript/FurnitureController.cs
@@ -4,10 +4,23 @@ public class FurnitureController : MonoBehaviour
 {
     //가구 컨트롤 스크립트
     private Furniture furnitureData;
-
-    public void Init(Furniture data)
+    private FurnitureSaveData furnitureSaveData;
+    public void Init(FurnitureSaveData data)
     {
-        furnitureData = data;
+        furnitureData = Resources.Load<Furniture>($"Data/Furniture/{data.id}");
 
+        if (furnitureData == null)
+        {
+            Debug.LogWarning($"고양이 데이터를 찾을 수 없습니다: {data.id}");
+            return;
+        }
+
+        // 2. 위치 설정 등
+        furnitureSaveData = data;
+        transform.position = furnitureSaveData.position;
+
+        Debug.Log($"가구 {furnitureData.furnitureId} 세팅 완료");
+
+        furnitureSaveData = data;
     }
 }
