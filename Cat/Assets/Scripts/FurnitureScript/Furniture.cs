757d10e0d9a671c58b515277894e2786be783d89
pjsjyh <pjsjyh@naver.com>
2025-07-28 15:03:35 +0900

    리소스 추가 및 변경

--- Cat/Assets/Scripts/FurnitureScript/Furniture.cs
+++ Cat/Assets/Scripts/FurnitureScript/Furniture.cs
@@ -9,6 +9,8 @@ public class Furniture : ScriptableObject
     public float installLocation; //설치된 방
     public Vector3 installPosition; //설치된 위치
     public bool isCatUse = false; //고양이 사용 여부
+
+    public Sprite FurnitureThumbnail; //가구 썸네일
 }
 
 [System.Serializable]
